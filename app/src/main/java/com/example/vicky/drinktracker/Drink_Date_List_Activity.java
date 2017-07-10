b2e2be6ba4d05a332c2bd8dd5363f6391fd24f88
vixplows <vicky@vickyplows.co.uk>
2017-07-11 00:49:08 +0100

    Add DrinkDateListActivity

---
+++ app/src/main/java/com/example/vicky/drinktracker/Drink_Date_List_Activity.java
@@ -0,0 +1,13 @@
+package com.example.vicky.drinktracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Drink_Date_List_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink__date__list_);
+    }
+}
