277309e16e263051576db87d7c9f86578f8f43ed
vixplows <vicky@vickyplows.co.uk>
2017-07-10 16:37:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/vicky/drinktracker/Drink.java
@@ -0,0 +1,22 @@
+package com.example.vicky.drinktracker;
+
+/**
+ * Created by vicky on 10/07/2017.
+ */
+
+public class Drink {
+
+    private DrinkType type;
+    private double cost;
+    private double volume;
+    private double strength;
+
+    public Drink(DrinkType type, double cost, double volume, double strength) {
+        this.type = type;
+        this.cost = cost;
+        this.volume = volume;
+        this.strength = strength;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/vicky/drinktracker/DrinkType.java
@@ -0,0 +1,13 @@
+package com.example.vicky.drinktracker;
+
+/**
+ * Created by vicky on 10/07/2017.
+ */
+
+public enum DrinkType {
+
+    BEER,
+    WINE,
+    SPIRIT
+
+}
